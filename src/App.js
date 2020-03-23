dafb743ad5a4309aaa8831e26b648e19d8d93d08
nikhil <nikhil@nikhil.com>
2020-03-23 21:51:57 +0530

    setting up alert

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
 	state = {
 		users: [],
-		loading: false
+		loading: false,
+		alert: null
 	};
 
 	//Search Github Users
@@ -30,6 +32,19 @@ class App extends Component {
 	//Clear Users from state
 	clearUsers = () => this.setState({ users: [], loading: false });
 
+	//Alerting the user when search is blank
+
+	setAlert = (msg, type) => {
+		this.setState({
+			alert: {
+				msg: msg,
+				type: type
+			}
+		});
+
+		setTimeout(() => this.setState({ alert: null }), 3000);
+	};
+
 	render() {
 		const { users, loading } = this.state;
 
@@ -37,10 +52,12 @@ class App extends Component {
 			<div className="App">
 				<Navbar />
 				<div className="container">
+					<Alert alert={this.state.alert} />
 					<Search
 						searchUsers={this.searchUsers}
 						clearUsers={this.clearUsers}
 						showClear={users.length > 0 ? true : false}
+						setAlert={this.setAlert}
 					/>
 					<Users loading={loading} users={users} />
 				</div>

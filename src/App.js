02cf4030af89a481608b472567913bc12fe9f8d1
nikhil <nikhil@nikhil.com>
2020-03-23 22:21:59 +0530

    Setting up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
+import About from './components/pages/About'
 
 class App extends Component {
 	state = {
@@ -49,19 +51,30 @@ class App extends Component {
 		const { users, loading } = this.state;
 
 		return (
+      <Router>
 			<div className="App">
 				<Navbar />
 				<div className="container">
-					<Alert alert={this.state.alert} />
-					<Search
+          <Alert alert={this.state.alert} />
+          <Switch>
+          <Route exact path='/' render={props => (
+            <Fragment>
+            <Search
 						searchUsers={this.searchUsers}
 						clearUsers={this.clearUsers}
 						showClear={users.length > 0 ? true : false}
 						setAlert={this.setAlert}
 					/>
+            </Fragment>
+            )
+          }/>
+          <Route exact path="/about" component={About}/>
+          </Switch>
+
 					<Users loading={loading} users={users} />
 				</div>
-			</div>
+      </div>
+      </Router>
 		);
 	}
 }

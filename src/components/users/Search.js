848ef7c03ec5b397eb8fe34f0de2ff5bd31b6250
nikhil <nikhil@nikhil.com>
2020-03-24 22:55:55 +0530

    cleaning up

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+import React, { useState, useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import GithubContext from '../../context/github/githubContext';
+
+const Search = () => {
+	const githubContext = useContext(GithubContext);
+	const alertContext = useContext(AlertContext);
 	const [ text, setText ] = useState('');
 
 	const onChange = (e) => setText(e.target.value);
@@ -8,9 +12,9 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 	const onSubmit = (e) => {
 		e.preventDefault();
 		if (text === '') {
-			setAlert('Please Enter something', 'light');
+			alertContext.setAlert('Please Enter something', 'light');
 		} else {
-			searchUsers(text);
+			githubContext.searchUsers(text);
 			setText('');
 		}
 	};
@@ -21,20 +25,13 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 				<input type="text" name="text" placeholder=" Search Users..." value={text} onChange={onChange} />
 				<input type="submit" value="Search" className="btn btn-dark btn-block" />
 			</form>
-			{showClear && (
-				<button className="btn btn-light btn-block" onClick={clearUsers}>
+			{githubContext.users.length > 0 && (
+				<button className="btn btn-light btn-block" onClick={githubContext.clearUsers}>
 					Clear
 				</button>
 			)}
 		</div>
 	);
-};
-
-Search.propTypes = {
-	searchUsers: PropTypes.func.isRequired,
-	clearUsers: PropTypes.func.isRequired,
-	showClear: PropTypes.bool.isRequired,
-	setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -15,14 +15,51 @@ const GithubState = (props) => {
 	const [ state, dispatch ] = useReducer(GithubReducer, initialState);
 
 	//Search  Users
+	const searchUsers = async (text) => {
+		setLoading();
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
 
-	//Get User
+		dispatch({
+			type: SEARCH_USERS,
+			payload: res.data.items
+		});
+	};
+
+	//Get a single Github user
+	const getUser = async (username) => {
+		setLoading();
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		dispatch({
+			type: GET_USER,
+			payload: res.data
+		});
+	};
 
 	//Get Repos
-
+	//get user's repoitories
+	const getUserRepos = async (username) => {
+		setLoading();
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=asc?client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		dispatch({
+			type: GET_REPOS,
+			payload: res.data
+		});
+	};
 	//Clear Users
+	const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
 	//Set Loading
+	const setLoading = () => dispatch({ type: SET_LOADING });
 
 	return (
 		<GithubContext.Provider
@@ -30,7 +67,11 @@ const GithubState = (props) => {
 				users: state.users,
 				user: state.user,
 				repos: state.repos,
-				loading: state.loading
+				loading: state.loading,
+				searchUsers,
+				clearUsers,
+				getUser,
+				getUserRepos
 			}}>
 			{props.children}
 		</GithubContext.Provider>
